78ea051537060c3f33d8944ae31bdfff6938d312
bach-long <nguyenbach19122002@gmail.com>
2023-09-16 15:50:09 +0700

    add config mail

--- backend/app/Http/Controllers/MangaController.php
+++ backend/app/Http/Controllers/MangaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Resources\Manga as MangaResource;
 use App\Models\Manga;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MangaController extends Controller
 {
@@ -19,8 +20,9 @@ class MangaController extends Controller
 
         if (! empty($category_ids)) {
             $query->whereHas('categories', function ($subQuery) use ($category_ids) {
-                $subQuery->whereIn('category_id', $category_ids);
-            });
+                $subQuery->select(DB::raw('count(distinct categories.id)'))
+                    ->whereIn('category_id', $category_ids);
+            }, '=', count($category_ids));
         }
 
         $mangas = $query->paginate($per_page, ['*'], 'page', $page);
