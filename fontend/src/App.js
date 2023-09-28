a3f95482d50cc2ccba39fcee97b79718b27c6b1a
bach-long <nguyenbach19122002@gmail.com>
2023-09-28 10:52:40 +0700

    test laravel echo

--- fontend/src/App.js
+++ fontend/src/App.js
@@ -8,11 +8,37 @@ import Guest from './pages/Guest';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import User from './pages/User';
+import Echo from 'laravel-echo';
+import Socketio from 'socket.io-client';
 
 function App() {
   const { authUser, setAuthUser } = useContext(AuthContext);
   const [token, setToken] = useState(localStorage.getItem('accessToken'));
   const [user, setUser] = useState({});
+
+
+  useEffect(() => {
+    window.io = require('socket.io-client');
+
+    window.Echo = new Echo({
+      client: Socketio,
+      broadcaster: "socket.io",
+      host: "http://localhost:6001",
+    })
+
+    window.Echo.connector.socket.on("connect", function () {
+      console.log("connect success")
+    })
+
+    window.Echo.connector.socket.on("disconnect", function () {
+      console.log("disconnected")
+    })
+
+    window.Echo.channel('laravel_database_my-channel')
+        .listen(".my-event", (e) => {
+          console.log(e);
+        });
+  }, []);
 
   useEffect(() => {
     setToken(localStorage.getItem('accessToken'));
@@ -20,7 +46,7 @@ function App() {
 
   useEffect(() => {
     setUser(JSON.parse(localStorage.getItem('auth-user')));
-    console.log(user);
+    //console.log(user);
   }, []);
 
   const role = authUser && authUser.role ? authUser.role : null;
