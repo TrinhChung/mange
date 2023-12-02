e089bd9762e5b0796ad1f4c422a42073a36ce71d
PainterHalver <hieptrang13042002@gmail.com>
2023-12-02 08:01:58 +0630

    add footer, ad data

--- fontend/src/layouts/HomeLayout/Footer.js
+++ fontend/src/layouts/HomeLayout/Footer.js
@@ -4,23 +4,27 @@ import './HomeLayout.scss';
 
 const FooterComponent = () => {
   const box1 = {
-    title: 'Lien he cho chung toi',
+    title: 'Web truyện tranh Mange',
     data: [
       'Điện thoại: 0812345678',
-      'Email: efpyi@example.com',
-      'Địa chỉ: 0812345678',
-      'Số điện thoại: 0812345678',
+      'Email: contact@uetvnu.id.vn',
+      'Địa chỉ: 144 Xuân Thủy, Cầu Giấy',
     ],
   };
 
   const box2 = {
-    title: 'zzz',
-    data: ['Anh Đào Đức Hiệp', 'yêu các em!'],
+    title: 'Môn học',
+    data: ['Tên: Thực hành phát triển phần mềm', 'Mã môn học: 2324I_INT3139_1', 'Bài tập lớn nhóm 2'],
   };
 
   const box3 = {
-    title: 'Danh cho cac em nam K66',
-    data: ['TIM NY', 'CHO TRAN ANH TU'],
+    title: 'Thành viên',
+    data: ['Trịnh Văn Chung', 'Trần Anh Tú', 'Nguyễn Xuân Bách', 'Đào Đức Hiệp', 'Lê Văn Hòa'],
+  };
+
+  const box4 = {
+    title: 'Mã nguồn ứng dụng',
+    data: ['https://github.com/TrinhChung/mange'],
   };
 
   const BoxContact = ({ title, data }) => {
@@ -39,12 +43,10 @@ const FooterComponent = () => {
     <div className="footer">
       <Col>
         <Row className="top-box">
-          <Col span={4}>{BoxContact(box1)}</Col>
-          <Col span={4}>{BoxContact(box2)}</Col>
-          <Col span={4}>{BoxContact(box3)}</Col>
-          <Col span={12}>
-            <Row className="title-box">Tham gia voi chung toi ngay luc nay</Row>
-          </Col>
+          <Col offset={2} span={5}>{BoxContact(box1)}</Col>
+          <Col span={5}>{BoxContact(box2)}</Col>
+          <Col span={5}>{BoxContact(box3)}</Col>
+          <Col span={5}>{BoxContact(box4)}</Col>
         </Row>
         <Row
           style={{
@@ -54,7 +56,7 @@ const FooterComponent = () => {
             alignItems: 'center',
           }}
         >
-          &#169;Ahihi
+          Copyright © 2023 Mange
         </Row>
       </Col>
     </div>
