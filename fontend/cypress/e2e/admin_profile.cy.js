9f177a732b179a563304ec18e7ce8e56d3f96ec9
TrinhChung <chungtrinh2k2@gmail.com>
2023-12-09 14:31:39 +0700

    fix cypress

--- fontend/cypress/e2e/admin_profile.cy.js
+++ fontend/cypress/e2e/admin_profile.cy.js
@@ -5,70 +5,83 @@ describe('Spec profile admin', () => {
     // set localStorage
     localStorage.setItem(
       'authUser',
-      JSON.stringify({ "id": 1, "username": "admin", "email": "diamond.hayes@example.com", "avatar": "https://via.placeholder.com/640x480.png/0044ff?text=reiciendis", "email_verified_at": "2023-11-05T06:25:18.000000Z", "role": "admin", "active": 1, "activated_at": "+055808-12-08T13:40:00.000000Z", "active_token": "cvAZwbm8EsmKTIOOlRh1", "reset_token": null, "reset_sent_at": null, "created_at": "2023-11-05T06:25:18.000000Z", "updated_at": "2023-11-05T06:25:18.000000Z" })
+      JSON.stringify({
+        id: 1,
+        username: 'admin',
+        email: 'diamond.hayes@example.com',
+        avatar:
+          'https://via.placeholder.com/640x480.png/0044ff?text=reiciendis',
+        email_verified_at: '2023-11-05T06:25:18.000000Z',
+        role: 'admin',
+        active: 1,
+        activated_at: '+055808-12-08T13:40:00.000000Z',
+        active_token: 'cvAZwbm8EsmKTIOOlRh1',
+        reset_token: null,
+        reset_sent_at: null,
+        created_at: '2023-11-05T06:25:18.000000Z',
+        updated_at: '2023-11-05T06:25:18.000000Z',
+      })
     );
 
     cy.visit('http://localhost:3000/profile');
   });
 
-  it('Vào được trang profile', () => { });
+  it('Vào được trang profile', () => {});
 
-  it("Có các mục tên tài khoản, email, trạng thái", () => {
+  it('Có các mục tên tài khoản, email, trạng thái', () => {
     cy.contains('Tên tài khoản');
     cy.contains('mail');
     cy.contains('Trạng thái');
-  })
+  });
 
-  it("Có các mục quản lý", () => {
+  it('Có các mục quản lý', () => {
     cy.contains('Quản lý truyện');
     cy.contains('Quản lý người dùng');
-  })
+  });
 
-  it("Có thể chuyển hướng tới quản lý người dùng", () => {
+  it('Có thể chuyển hướng tới quản lý người dùng', () => {
     cy.contains('Quản lý người dùng').click();
     cy.url().should('include', '/management-account');
-  })
+  });
 
-  it("Có thể chuyển hướng tới quản lý truyện", () => {
-    cy.contains('Quản lý truyện').click();
-    cy.url().should('include', '/management-story');
-  })
-
-  it("Có thể chuyển hướng tới quản lý bình luận bị báo cáo", () => {
+  it('Có thể chuyển hướng tới quản lý bình luận bị báo cáo', () => {
     cy.contains('Bình luận bị báo cáo').click();
     cy.url().should('include', '/management-comment');
-  })
+  });
 
-  it("Có thể chuyển hướng tới màn thêm truyện mới", () => {
+  it('Có thể chuyển hướng tới màn thêm truyện mới', () => {
     cy.contains('Thêm truyện mới').click();
     cy.url().should('include', '/post');
-  })
+  });
 
-  it("Có thể chọn Upload Ảnh", () => {
+  it('Có thể chọn Upload Ảnh', () => {
     cy.contains('Upload Ảnh').click();
-    cy.contains("Thay đổi ảnh đại diện");
-  })
+    cy.contains('Thay đổi ảnh đại diện');
+  });
 
-  it("Có thể Upload ảnh đại diện", () => {
+  it('Có thể Upload ảnh đại diện', () => {
     cy.intercept('POST', '/api/me/avatar', {
       statusCode: 200,
       fixture: 'profile/upload_avatar_success.json',
     }).as('avatar');
 
     cy.contains('Upload Ảnh').click();
-    cy.contains("Chọn ảnh").click();
-    cy.get('input[type="file"]').selectFile({
-      contents: Cypress.Buffer.from('file contents'),
-      fileName: 'avatar.jpg',
-      mimeType: 'image/jpeg'
-    }, { force: true })
+    cy.contains('Chọn ảnh').click();
+    cy.get('input[type="file"]').selectFile(
+      {
+        contents: Cypress.Buffer.from('file contents'),
+        fileName: 'avatar.jpg',
+        mimeType: 'image/jpeg',
+      },
+      { force: true }
+    );
     cy.contains('OK').click();
 
     cy.contains('thành công');
-  })
+  });
 
-  it("Có thể hiển thị các bình luận bị báo cáo", () => {
+  it('Có thể hiển thị các bình luận bị báo cáo', () => {
     cy.contains('Bình luận bị báo cáo').click();
     // TODO: Add logic
-  })
+  });
 });
